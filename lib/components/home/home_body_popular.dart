aabb082823ff2567caf06c883d7dacb779847988
basakk123 <basakk@naver.com>
2022-11-10 16:35:30 +0900

    바디에 배너 만들기 완료

---
+++ lib/components/home/home_body_popular.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_airbnb/components/home/home_body_popular_item.dart';
+
+class HomeBodyPopular extends StatelessWidget {
+  const HomeBodyPopular({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildPopularTitle(),
+        _buildPopularList(),
+      ],
+    );
+  }
+
+  Widget _buildPopularTitle() {
+    return SizedBox();
+  }
+
+  Widget _buildPopularList() {
+    // 나중에 Container 빼기
+    return Container(
+      color: Colors.red,
+      // 나중에 Row로 변경해보기
+      child: Wrap(
+        children: [
+          HomeBodyPopularItem(id: 0),
+          SizedBox(
+            width: 7.5,
+          ),
+          HomeBodyPopularItem(id: 1),
+          SizedBox(
+            width: 7.5,
+          ),
+          HomeBodyPopularItem(id: 2),
+        ],
+      ),
+    );
+  }
+}
